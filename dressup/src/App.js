1a5688f4aff3149ca4257ee7f75f09eaf82c67d7
varun-Tad <varuntad141@gmail.com>
2022-03-23 14:14:18 +0530

    removed Products page

--- dressup/src/App.js
+++ dressup/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
-import Productspage from "./pages/ProductsPage/Productspage";
 
 function App() {
   return (
     <div className="App">
       <Homepage />
-      <Productspage />
     </div>
   );
 }
--- dressup/src/pages/ProductsPage/Productspage.js
+++
@@ -1,9 +0,0 @@
-const Productspage = () => {
-  return (
-    <div>
-      <h2>Hello</h2>
-    </div>
-  );
-};
-
-export default Productspage;
